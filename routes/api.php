de600db026ec028aaaec8795d62adfc2573ba1ba
push0012 <pushpamal0012@gmail.com>
2019-07-23 08:29:14 +0530

    insert login and register routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('login', 'Auth\LoginController@login');
+Route::post('register', 'Auth\RegisterController@register');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
