efbd912557bafde3386ea79577ab262782f117ac
push0012 <pushpamal0012@gmail.com>
2019-10-01 15:48:53 +0530

    create administration dashboard

--- src/pages/Maps.vue
+++ src/pages/Maps.vue
@@ -1,14 +1,61 @@
 <template>
-  <gmap-map
-    id="map"
-    :center="center"
-    :zoom="13"
-    :options="options"
-    map-type-id="terrain"
-  >
-    <gmap-marker :position="center">
-    </gmap-marker>
-  </gmap-map>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <card>
+            <template slot="header">
+              <h4 class="card-title">Administration</h4>
+              <p class="card-category"></p>
+            </template>
+            <div class="all-icons">
+              <div class="row">
+                <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-6">
+                  <a href="#/admin/user/create" >
+                  <div class="font-icon-detail" >
+                    <i class="nc-icon nc-circle-09"></i>
+                    <p class="text-primary">Create User</p>
+                  </div>
+                  </a>
+                </div>
+                <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-6">
+                  <div class="font-icon-detail">
+                    <i class="nc-icon nc-badge"></i>
+                     <p class="text-primary">View Users</p>
+                  </div>
+                </div>
+                <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-6">
+                  <div class="font-icon-detail">
+                    <i class="nc-icon nc-alien-33"></i>
+                    <p>nc-alien-33</p>
+                  </div>
+                </div>
+                <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-6">
+                  <div class="font-icon-detail">
+                    <i class="nc-icon nc-align-center"></i>
+                    <p>nc-align-center</p>
+                  </div>
+                </div>
+                <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-6">
+                  <div class="font-icon-detail">
+                    <i class="nc-icon nc-align-left-2"></i>
+                    <p>nc-align-left-2</p>
+                  </div>
+                </div>
+                <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-6">
+                  <div class="font-icon-detail">
+                    <i class="nc-icon nc-ambulance"></i>
+                    <p>nc-ambulance</p>
+                  </div>
+                </div>
+                </div>
+            </div>
+          </card>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 <script>
   import {API_KEY} from './Maps/API_KEY'
@@ -73,5 +120,8 @@
 <style>
   #map {
     min-height: calc(100vh - 123px);
+  }
+  a {
+    text-decoration: none !important;
   }
 </style>
