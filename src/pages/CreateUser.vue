9e447386b44dd505fe16e1f477a1daed24c37b95
push0012 <pushpamal0012@gmail.com>
2019-10-01 15:49:28 +0530

    first initialization

---
+++ src/pages/CreateUser.vue
@@ -0,0 +1,102 @@
+<template>
+<div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-8">
+  <card>
+    <h4 slot="header" class="card-title">Create User</h4>
+    <form>
+      <div class="row">
+        <div class="col-md-8">
+          <base-input type="text"
+                    label="Name"
+                    placeholder="Name"
+                    v-model="user.name">
+          </base-input>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-md-8">
+          <base-input type="email"
+                    label="Email"
+                    placeholder="Email"
+                    v-model="user.email">
+          </base-input>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+            <label >User Type</label>
+            <b-form-select v-model="user.selected" :options="option"></b-form-select>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <base-input type="password"
+                    label="password"
+                    placeholder="Password"
+                    v-model="user.password">
+          </base-input>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-md-8">
+          <base-input type="password"
+                    label="password confirmation"
+                    placeholder="Password Confirmation"
+                    v-model="user.password_confirmation">
+          </base-input>
+        </div>
+      </div>
+      <div class="text-center">
+        <button type="submit" class="btn btn-info btn-fill float-right" @click.prevent="createUser">
+          Create User
+        </button>
+      </div>
+      <div class="clearfix"></div>
+    </form>
+  </card>
+  </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card,
+    },
+    data () {
+      return {
+        user: {
+          name: '',
+          email: '',
+          selected: null,
+          password:'',
+          password_confirmation:''
+        },
+        //selected: null,
+        option: [
+          { value: null, text: 'Please select an option', disabled: true },
+          { value: 'developer', text: 'Developer' },
+          { value: 'admin', text: 'Administrator' },
+          { value: 'super_user', text: 'Super User' },
+          { value: 'user', text: 'User' }
+        ]
+      }
+    },
+    methods: {
+      createUser () {
+         alert('Your data: ' + JSON.stringify(this.user))
+      }
+    }
+  }
+
+</script>
+<style>
+.drops{
+    text-decoration: none;
+}
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="wrapper">
+    <div class="bodi">
+ <div class="container containers">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card cardx">
+			<div class="card-header card-headerx">
+				<h3>Sign In</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span>G</span>
+          			<span>L</span>
+          			<span>M</span>
+          			<span>S</span>
+				</div>
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fa fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control"  placeholder="username">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fa fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control"  placeholder="password">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox">Remember Me
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Login" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+</div>
+</template>
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+  export default {
+    components: {
+      Card
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+      updateProfile () {
+        alert('Your data: ' + JSON.stringify(this.user))
+      }
+    }
+  }
+</script>
+<style>
+
+
+.bodi{
+background-image: url('http://getwallpapers.com/wallpaper/full/a/5/d/544750.jpg');
+background-size: cover;
+background-repeat: no-repeat;
+height: 100%;
+font-family: 'Numans', sans-serif;
+}
+
+.containers{
+height: 100%;
+align-content: center;
+}
+
+.cardx{
+height: 370px;
+margin-top: auto;
+margin-bottom: auto;
+width: 400px;
+background-color: rgba(0,0,0,0.5) !important;
+}
+.card-headerx{
+  background-color: transparent !important;
+}
+
+.social_icon span{
+font-size: 60px;
+margin-left: 15px;
+color: #FFC312;
+}
+
+.social_icon span:hover{
+color: white;
+cursor: pointer;
+transform: rotateZ(-30deg);
+}
+
+.card-headerx h3{
+color: white;
+}
+
+.social_icon{
+position: absolute;
+right: 20px;
+top: -45px;
+}
+
+.input-group-prepend span{
+width: 50px;
+background-color: #FFC312;
+color: black;
+border:0 !important;
+}
+
+input:focus{
+outline: 0 0 0 0  !important;
+box-shadow: 0 0 0 0 !important;
+
+}
+
+.remember{
+color: white;
+}
+
+.remember input
+{
+width: 20px;
+height: 20px;
+margin-left: 15px;
+margin-right: 5px;
+}
+
+.login_btn{
+color: black;
+background-color: #FFC312;
+width: 100px;
+}
+
+.login_btn:hover{
+color: black;
+background-color: white;
+}
+
+.links{
+color: white;
+}
+
+.links a{
+margin-left: 4px;
+}
+</style>
