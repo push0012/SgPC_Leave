d76cebfcb83309df70de6af240890cd68d7c2673
push0012 <pushpamal0012@gmail.com>
2019-10-01 15:43:49 +0530

    import some libraries and configured

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import vSelect from 'vue-select'
+import BootstrapVue from 'bootstrap-vue'
 // LightBootstrap plugin
 import LightBootstrap from './light-bootstrap-main'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 // router setup
 import routes from './routes/routes'
 
 import './registerServiceWorker'
+
+
+
 // plugin setup
+Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
 Vue.use(LightBootstrap)
+Vue.use(BootstrapVue)
+Vue.component('v-select', vSelect)
 
 // configure router
 const router = new VueRouter({
