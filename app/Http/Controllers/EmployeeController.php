61597b05412546def289cab10a9e3151159c8e67
push0012 <pushpamal0012@gmail.com>
2019-10-01 15:55:25 +0530

    add construct method with middleware and scopes

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,6 +7,10 @@ use App\Employee;
 
 class EmployeeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth:api', 'scopes:edit,create,delete,view'])->except(['index', 'show']);
+    }
     /**
      * Display a listing of the resource.
      *
