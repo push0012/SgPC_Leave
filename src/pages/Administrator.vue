43d01041ce8931ae2a66ef1e2951a38f1cd63656
push0012 <pushpamal0012@gmail.com>
2019-10-02 09:46:00 +0530

    renamed Maps page to Administrator page

--- src/pages/Administrator.vue
+++ src/pages/Administrator.vue
@@ -11,7 +11,7 @@
             <div class="all-icons">
               <div class="row">
                 <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-6">
-                  <a href="#/admin/user/create" >
+                  <a href="#/admin/admin/user/create" >
                   <div class="font-icon-detail" >
                     <i class="nc-icon nc-circle-09"></i>
                     <p class="text-primary">Create User</p>
@@ -58,70 +58,14 @@
   </div>
 </template>
 <script>
-  import {API_KEY} from './Maps/API_KEY'
-  import Vue from 'vue'
-  import * as VueGoogleMaps from 'vue2-google-maps'
-  Vue.use(VueGoogleMaps, {
-    load: {
-      key: API_KEY
-    }
-  })
+  
   export default {
     data () {
       return {
-        center: {
-          lat: 40.748817,
-          lng: -73.985428
-        },
-        options: {
-          styles: [{
-            'featureType': 'water',
-            'stylers': [{'saturation': 43}, {'lightness': -11}, {'hue': '#0088ff'}]
-          }, {
-            'featureType': 'road',
-            'elementType': 'geometry.fill',
-            'stylers': [{'hue': '#ff0000'}, {'saturation': -100}, {'lightness': 99}]
-          }, {
-            'featureType': 'road',
-            'elementType': 'geometry.stroke',
-            'stylers': [{'color': '#808080'}, {'lightness': 54}]
-          }, {
-            'featureType': 'landscape.man_made',
-            'elementType': 'geometry.fill',
-            'stylers': [{'color': '#ece2d9'}]
-          }, {
-            'featureType': 'poi.park',
-            'elementType': 'geometry.fill',
-            'stylers': [{'color': '#ccdca1'}]
-          }, {
-            'featureType': 'road',
-            'elementType': 'labels.text.fill',
-            'stylers': [{'color': '#767676'}]
-          }, {
-            'featureType': 'road',
-            'elementType': 'labels.text.stroke',
-            'stylers': [{'color': '#ffffff'}]
-          }, {'featureType': 'poi', 'stylers': [{'visibility': 'off'}]}, {
-            'featureType': 'landscape.natural',
-            'elementType': 'geometry.fill',
-            'stylers': [{'visibility': 'on'}, {'color': '#b8cb93'}]
-          }, {'featureType': 'poi.park', 'stylers': [{'visibility': 'on'}]}, {
-            'featureType': 'poi.sports_complex',
-            'stylers': [{'visibility': 'on'}]
-          }, {'featureType': 'poi.medical', 'stylers': [{'visibility': 'on'}]}, {
-            'featureType': 'poi.business',
-            'stylers': [{'visibility': 'simplified'}]
-          }]
-        }
+        
       }
     }
   }
 </script>
 <style>
-  #map {
-    min-height: calc(100vh - 123px);
-  }
-  a {
-    text-decoration: none !important;
-  }
 </style>
