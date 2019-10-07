ffbcfdc97cd42d4cbf85d707e2fe8d3dcbb3744f
push0012 <pushpamal0012@gmail.com>
2019-10-07 15:41:37 +0530

    create and commented some sidebar-links

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -6,37 +6,47 @@
         <i class="nc-icon nc-chart-pie-35"></i>
         <p>Dashboard</p>
       </sidebar-link>
+
       <sidebar-link to="/admin/user">
         <i class="nc-icon nc-circle-09"></i>
         <p>User Profile</p>
-      </sidebar-link>
+      </sidebar-link> 
+
+      <!--<sidebar-link to="/admin/user">
+        <i class="nc-icon nc-simple-add"></i>
+        <p>Apply Leave</p>
+      </sidebar-link>-->
+
       <sidebar-link to="/admin/table-list">
         <i class="nc-icon nc-notes"></i>
         <p>Table list</p>
       </sidebar-link>
+
       <sidebar-link to="/admin/typography">
         <i class="nc-icon nc-paper-2"></i>
         <p>Typography</p>
       </sidebar-link>
+
       <sidebar-link to="/admin/icons">
         <i class="nc-icon nc-atom"></i>
         <p>Icons</p>
       </sidebar-link>
-      <sidebar-link to="/admin/admin">
-        <i class="nc-icon nc-settings-gear-64"></i>
-        <p>Administration</p>
-      </sidebar-link>
+
       <sidebar-link to="/admin/notifications">
         <i class="nc-icon nc-bell-55"></i>
         <p>Notifications</p>
       </sidebar-link>
 
       <template slot="bottom-links">
-        <sidebar-link class="active"
+        <sidebar-link to="/admin/admin">
+        <i class="nc-icon nc-settings-gear-64"></i>
+        <p>Administration</p>
+      </sidebar-link>
+        <!--<sidebar-link class="active"
                       to="/admin/upgrade">
           <i class="nc-icon nc-alien-33"></i>
           <p>Upgrade to PRO</p>
-        </sidebar-link>
+        </sidebar-link>-->
       </template>
     </side-bar>
     <div class="main-panel">
