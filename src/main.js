d625e1bfab74e5b11a91b67adace082cb9500e16
push0012 <pushpamal0012@gmail.com>
2019-10-28 15:05:31 +0530

    changed some token things

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import store from './store'
+//import routers from './routes/routes'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import vSelect from 'vue-select'
@@ -21,13 +23,15 @@ Vue.use(FlashMessage);
 // plugin setup
 Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
+//Vue.use(router)
+//Vue.use(Store)
 Vue.use(LightBootstrap)
 Vue.use(BootstrapVue)
 Vue.component('v-select', vSelect)
 
 // configure router
 const router = new VueRouter({
-  routes, // short for routes: routes
+  routes,
   linkActiveClass: 'nav-item active',
   scrollBehavior: (to) => {
     if (to.hash) {
@@ -38,9 +42,16 @@ const router = new VueRouter({
   }
 })
 
+//Vue.prototype.$http = axios;
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App),
-  router
 })
